720808230e79d4053490c792c7a9d797f29ca0a3
kapil-122414 <kapilkumar122414@gmail.com>
2026-04-01 15:03:20 +0530

    config

--- backend/config/bd.js
+++ backend/config/bd.js
@@ -2,7 +2,7 @@ const mongoose=require ("mongoose");
 
 const connectdb= async() => {
     try{
-        await mongoose.connect("mongodb://localhost:27017/caregorydb")
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("databse connected");
     }
     catch(error){
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+ require("dotenv").config();
 const connectdb = require("./config/bd");
 const routes=require('./Routes/Routers');
 
@@ -7,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 connectdb();
 const cors = require("cors");
-app.use(cors());
+app.use(cors({origin:"*"}));
 const port = process.env.PORT || 5000;
 app.use('/api',routes);
 app.use("/uploads", express.static("uploads"));
