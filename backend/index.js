a986b77cd98dcc1d7cd8b00afb8236d3476de563
kapil-122414 <kapilkumar122414@gmail.com>
2026-04-01 15:04:11 +0530

    new

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,19 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
- require("dotenv").config();
+
 const connectdb = require("./config/bd");
-const routes=require('./Routes/Routers');
+const routes = require("./Routes/Routers");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 connectdb();
 const cors = require("cors");
-app.use(cors({origin:"*"}));
+app.use(cors({ origin: "*" }));
 const port = process.env.PORT || 5000;
-app.use('/api',routes);
+app.use("/api", routes);
 app.use("/uploads", express.static("uploads"));
-   
 
 app.get("/api", (req, res) => {
   console.log("hyy");
