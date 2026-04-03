5e885f3afeddb48d86c9b0a503f194c2c96d0d52
kapil-122414 <kapilkumar122414@gmail.com>
2026-04-03 16:48:24 +0530

    fix the cloudinary

--- backend/multer/imgmulter.js
+++ backend/multer/imgmulter.js
@@ -1,12 +1,12 @@
 const multer = require("multer");
-const {CloudinaryStorage}=require('multer-storage-cloudinary');
-const cloudinary=require('../config/cloudinary');
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("../config/cloudinary");
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: "categories",
-    allowed_formats: ["jpg", "png", "jpeg"],
+    resource_type: "image",
   },
 });
 
