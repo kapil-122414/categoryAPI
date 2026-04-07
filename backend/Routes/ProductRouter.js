26ab5d0c69e9568675ff57b9c96341310d751780
kapil-122414 <kapilkumar122414@gmail.com>
2026-04-07 12:58:29 +0530

    update api create

--- backend/Routes/ProductRouter.js
+++ backend/Routes/ProductRouter.js
@@ -62,4 +62,32 @@ router.delete("/product/:_id", async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+
+router.patch("/product/:_id", uploads.single("Img"), async (req, res) => {
+  try {
+    const id = req.params._id;
+    const olddata = await productschema.findById(id);
+
+    if (!olddata) {
+      return res.status(404).json({ message: "not found" });
+    }
+
+    const updatedata = { ...req.body };
+
+    if (req.file) {
+      if (olddata.Img) {
+        const publicId = olddata.Img.split("/").pop().split(".")[0];
+        await cloudinary.uploader.destroy(`products/${publicId}`);
+      }
+      updatedata.Img = req.file.path;
+    }
+    const newdata = await productschema.findByIdAndUpdate(id, updatedata, {
+      new: true,
+    });
+    console.log(newdata);
+    res.status(200).json({ message: "successfully", newdata });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 module.exports = router;
