c74a0a0f5f46938cbcb63df790d20f539f475ea4
kapil-122414 <kapilkumar122414@gmail.com>
2026-04-13 17:45:23 +0530

    order get ,post or delete api

--- backend/Routes/orderdRouter.js
+++ backend/Routes/orderdRouter.js
@@ -9,7 +9,6 @@ router.post("/order", authmiddleware, async (req, res) => {
     const userid = req.user.id;
     const cartitems = await carts
       .find({ UserId: userid })
-
       .populate("ProductId");
     if (!cartitems.length) {
       return res.status(404).json({ message: "cartitems is not" });
@@ -21,7 +20,7 @@ router.post("/order", authmiddleware, async (req, res) => {
 
       console.log(totalamount);
       return {
-        product: item.ProductId,
+        product: item.ProductId._id,
         name: item.ProductId?.Productname,
         price: item.ProductId.price,
         Quantity: item.Quantity,
@@ -34,9 +33,77 @@ router.post("/order", authmiddleware, async (req, res) => {
       totalamount,
     });
     const deletdata = await carts.deleteMany({ UserId: userid });
-    console.log(deletdata);
 
     res.status(200).json({ message: "orderd place successfuly", newOrder });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.get("/order", authmiddleware, async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+    const search = req.query.search || "";
+    const status = req.query.status || "";
+    const filter = {
+      userid: req.user.id,
+    };
+    if (status) {
+      filter.status = status;
+    }
+    if (search) {
+      filter["items.name"] = { $regex: search, $options: "i" };
+    }
+
+    const data = await orders
+      .find(filter)
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit);
+    const total = await orders.countDocuments(filter);
+    res.status(200).json({
+      message: "success",
+      data,
+      page,
+      totalPages: Math.ceil(total / limit),
+      totalData: total,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.delete("/order/:id", authmiddleware, async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const deletedta = await orders.findByIdAndDelete(id);
+
+    res.status(200).json({ message: "successfuly", deletedta });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+router.patch("/order", authmiddleware, async (req, res) => {
+  try {
+    const validation = ["placed", "shipped", "deleverd", "cencelled"];
+    const orderid = req.params.id;
+    const { status } = req.body;
+    const updateorder = await orders.findOneAndUpdate(
+      { id: orderid, userid: req.user.id },
+      { status },
+      { new: true },
+    );
+
+    if (!updateorder) {
+      return res.status(404).json({ message: "Order not found" });
+    }
+    if (!validation.includes(status)) {
+      return res.status(400).json({ message: "Invalid status" });
+    }
+    res.status(200).json({ message: "success", updateorder });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
